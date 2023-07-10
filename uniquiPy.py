cc0cb433d16358bcf7b0daba74ed09cd817e0100
darkmatterpropulsion <lorenzodellavolpe@gmail.com>
2023-07-10 13:27:41 +0200

    Working version

--- uniquiPy.py
+++ uniquiPy.py
@@ -11,11 +11,16 @@ BLOCK_SIZE = 65536
  The hash is calculated in blocks to avoid reading large files that could potentially fill the memory and crash the program.'''
 def fingerprint(path):
     hash_method = hashlib.sha256()
-    with open(path, "rb") as input_file:
-        buf = input_file.read(BLOCK_SIZE)
-        while len(buf) > 0:
-            hash_method.update(buf)
+
+    try:
+        with open(path, "rb") as input_file:
             buf = input_file.read(BLOCK_SIZE)
+            while len(buf) > 0:
+                hash_method.update(buf)
+                buf = input_file.read(BLOCK_SIZE)
+    except Exception as e:
+        print(f"[-] An error occurred while opening {path} for hashing: " + e)
+        return ''
     return hash_method.hexdigest()
 
 
@@ -75,15 +80,11 @@ def create_dir(path):
         try:
             os.mkdir(path)
             return []
-        except PermissionError:
-            print(
-                "it appears that you don't have the permission to access "
-                + path
-                + ". Skipping..."
-            )
-            return []
         except FileExistsError:
-            print("It appears that the file already exists but was not detected.")
+            print("[!] It appears that the file already exists but was not detected.")
+            return generate_folder_hash(path)
+        except Exception as e:
+            print(f"[-] An error has occurred while checking existence of {path}" + e)
             return []
     else:
         return generate_folder_hash(path)
@@ -113,17 +114,18 @@ def parse_configuration(data):
  them in a list
 '''
 def search_for_files(paths_to_check):
-    all_the_paths = []
+    all_the_files = []
     for path in paths_to_check:
-        all_the_paths.extend(search_path(path))
-    return all_the_paths
+        all_the_files.extend(search_path(path))
+    return all_the_files
+
 
 '''
- This last block check if each file in all_the_paths is unique and if it is so,
+ This last block check if each file in all_the_files is unique and if it is so,
  it is written to the directory where it belong.
 '''
-def write_files(all_the_paths, data, hashes, path_to_write):
-    for path in all_the_paths:
+def write_files(all_the_files, data, hashes, path_to_write):
+    for path in all_the_files:
         file_name, file_extension = get_file_name_and_extension(path)
         for item in data["extensions"]:
             for key, values in item.items():
@@ -171,17 +173,22 @@ def main():
 
     paths_to_check = []
     path_to_write = []
-    all_the_paths = []
+    all_the_files = []
     hashes = {}
 
-	
-    with open("uniquiPy.yaml", "r") as f:
-        data = yaml.safe_load(f)
-        hashes, path_to_write, paths_to_check = parse_configuration(data)
+    try:
+        with open("uniquiPy.yaml", "r") as f:
+            data = yaml.safe_load(f)
+            hashes, path_to_write, paths_to_check = parse_configuration(data)
+    except FileNotFoundError:
+        print("[-] The configuration file uniquiPy.yaml was not found.")
+        sys.exit(-1)
+    except Exception as e:
+        print("An error has occurred while reading the configuration file: " + e)
+        
+    all_the_files = search_for_files(paths_to_check)
 
-    all_the_paths = search_for_files(paths_to_check)
-
-    write_files(all_the_paths, data, hashes, path_to_write)
+    write_files(all_the_files, data, hashes, path_to_write)
 
 
 if __name__ == "__main__":
